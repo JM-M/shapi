104ae6f0d22d5f20771074256c2d5b44cfac35f8
Michael Jola-Moses <jolamoses@gmail.com>
2025-09-26 10:12:40 +0100

    fix buildtime errors

--- src/site.config.ts
+++ src/site.config.ts
@@ -36,7 +36,7 @@ const siteConfig = {
         index: true,
         follow: true,
         "max-video-preview": -1,
-        "max-image-preview": "large",
+        "max-image-preview": "large" as const,
         "max-snippet": -1,
       },
     },
